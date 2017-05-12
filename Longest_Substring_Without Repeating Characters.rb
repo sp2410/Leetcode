041418164a933e35b91971ec6187f55045f74e43
saurav prakash <saprakas@syr.edu>
2017-05-12 12:02:13 -0700

    Longest Substring Without Repeating Characters
    Given a string, find the length of the longest substring without repeating characters.
    
    Examples:
    
    Given "abcabcbb", the answer is "abc", which the length is 3.
    
    Given "bbbbb", the answer is "b", with the length of 1.
    
    Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.

---
+++ Longest_Substring_Without Repeating Characters.rb
@@ -0,0 +1,42 @@
+#This solution is exceeding time limit, work on better 
+
+def length_of_longest_substring(str)
+  
+  if !str 
+    return 0
+  end 
+  
+  s_len = str.length
+  
+  if s_len == 0
+    return 0
+  end
+  
+  
+  i = 0 
+  len = 0 
+  maxlen = 0
+  myhash = Hash.new 
+  
+  while (i < s_len )
+    if ! myhash.key? str[i]
+      len = len+1 
+      myhash[str[i]] = i 
+      
+      if maxlen < len 
+        maxlen = len
+      end 
+      
+    else
+      i = myhash[str[i]]
+      len = 0 
+      myhash = Hash.new
+    end 
+    
+    i= i+1
+    
+  end
+  
+  return maxlen
+  
+end 
